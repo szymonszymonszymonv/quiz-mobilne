5f29a5855b16f74d53550ddb96b14f46ef010a2b
szymonszymonszymonv <kalarepka8799@gmail.com>
2021-11-15 21:56:16 +0100

    add prompts

--- app/src/main/java/pl/edu/pb/wi/MainActivity.java
+++ app/src/main/java/pl/edu/pb/wi/MainActivity.java
@@ -1,8 +1,13 @@
 package pl.edu.pb.wi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,9 +18,15 @@ public class MainActivity extends AppCompatActivity {
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button promptButton;
     private TextView questionTextView;
+    private boolean answerWasShown;
+    private static final String QUIZ_TAG = "MainActivity";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.Quiz.correctAnswer";
+    public static final int REQUEST_CODE_PROMPT = 0;
 
-    private Question[] questions = new Question[] {
+    private Question[] questions = new Question[]{
             new Question(R.string.q_activity, true),
             new Question(R.string.q_find_resources, false),
             new Question(R.string.q_listener, true),
@@ -28,10 +39,12 @@ public class MainActivity extends AppCompatActivity {
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if(userAnswer == correctAnswer) {
-            resultMessageId = R.string.correct_answer;
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
         }
-        else {
+        if (userAnswer == correctAnswer) {
+            resultMessageId = R.string.correct_answer;
+        } else {
             resultMessageId = R.string.incorrect_answer;
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
@@ -39,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setNextQuestion() {
         this.currentIndex = (this.currentIndex + 1) % questions.length;
+        answerWasShown = false;
         questionTextView.setText(questions[currentIndex].getQuestionId());
     }
 
@@ -46,29 +60,85 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.question_text_view);
+        promptButton = findViewById(R.id.prompt_button);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswerCorrectness(true);
             }
         });
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswerCorrectness(false);
             }
         });
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 setNextQuestion();
             }
         });
+        promptButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG, "wywołano: onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG, "wywołano: onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG, "wywołano: onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG, "wywołano: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG, "wywołano: onDestroy()");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {return; }
+        if (requestCode == REQUEST_CODE_PROMPT){
+            if (data == null) {return; }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
     }
 }
